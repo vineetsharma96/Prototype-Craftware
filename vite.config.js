2490c0737db4f7e267e8b6eb7d1352badc90879f
Vineet Sharma <vineet9650022810@gmail.com>
2026-09-09 19:00:03 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
 })
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/Prototype-Craftware/',
+  plugins: [react()],
+})
