f6c464d65d30963844ca45f54b16c0de56f738f0
Vineet Sharma <vineet9650022810@gmail.com>
2026-09-10 07:30:31 +0530

    Update Vite config for base path and output directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Prototype-Craftware/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
